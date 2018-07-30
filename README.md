a82af5aa8eacc8d2dc8588dbb6c23eb328fc8e12
Till Krüss <till@kruss.io>
2018-07-30 11:51:08 -0700

    import code

---
+++ README.md
@@ -0,0 +1,19 @@
+# Laravel Docs Workflow for Alfred
+
+An ultra-fast Laravel docs search workflow for Alfred 3.
+
+![Screenshot](screenshot.png)
+
+## Installation
+
+1. [Download the latest version](https://github.com/tillkruss/alfred-laravel-docs/releases/download/v0.1.0/Laravel.Docs.alfredworkflow)
+2. Install the workflow by double-clicking the `.alfredworkflow` file
+3. You can add the workflow to a category, then click "Import" to finish importing. You'll now see the workflow listed in the left sidebar of your Workflows preferences pane.
+
+## Usage
+
+```
+ld <query>
+```
+
+Just type `ld` followed by your search query.
---
+++ laravel.php
@@ -0,0 +1,63 @@
+<?php
+
+use Alfred\Workflows\Workflow;
+
+use AlgoliaSearch\Client as Algolia;
+use AlgoliaSearch\Version as AlgoliaUserAgent;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$query = $argv[1];
+$branch = $_ENV['branch'] ?? '5.6';
+
+$workflow = new Workflow;
+$parsedown = new Parsedown;
+$algolia = new Algolia('8BB87I11DE', '8e1d446d61fce359f69cd7c8b86a50de');
+
+AlgoliaUserAgent::addSuffixUserAgentSegment('Alfred Workflow', '0.1.0');
+
+$index = $algolia->initIndex('docs');
+$search = $index->search($query, ['tagFilters' => $branch]);
+$results = $search['hits'];
+
+if (empty($results)) {
+    $workflow->result()
+        ->title('No matches')
+        ->icon('google.png')
+        ->subtitle("No match found in the {$branch} docs. Search Google for: \"{$query}\"")
+        ->arg("https://www.google.com/search?q={$query}")
+        ->quicklookurl("https://www.google.com/search?q={$query}")
+        ->valid(true);
+
+    echo $workflow->output();
+    exit;
+}
+
+foreach ($results as $hit) {
+    $hasText = isset($hit['_highlightResult']['content']['value']);
+    $hasSubtitle = isset($hit['h2']);
+
+    $title = $hit['h1'];
+    $subtitle = $hasSubtitle ? $hit['h2'] : null;
+
+    if ($hasSubtitle && $hasText) {
+        $title = $hit['h2'];
+        $subtitle = $hit['_highlightResult']['content']['value'];
+    }
+
+    $title = strip_tags(html_entity_decode($title, ENT_QUOTES, 'UTF-8'));
+
+    $subtitle = $parsedown->line($subtitle);
+    $subtitle = strip_tags(html_entity_decode($subtitle, ENT_QUOTES, 'UTF-8'));
+
+    $workflow->result()
+        ->uid($hit['objectID'])
+        ->title($title)
+        ->autocomplete($title)
+        ->subtitle($subtitle)
+        ->arg("https://laravel.com/docs/{$branch}/{$hit['link']}")
+        ->quicklookurl("https://laravel.com/docs/{$branch}/{$hit['link']}")
+        ->valid(true);
+}
+
+echo $workflow->output();
